b8ce88a26403615c08fca4c80182de21e0810b9f
Tobi <noggnzzz@gmail.com>
2024-11-13 21:25:13 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://1nfinit0.github.io/Portfolio',
-  base: '/1nfinit0',
-  build: { outDir: 'dist', // Directorio de salida 
-  },
+  site: 'https://1nfinit0.github.io',
+  base: '/portfolio-astro',
 });
